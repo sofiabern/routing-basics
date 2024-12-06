c9628fb71d720d449c47736d83547cd6a5e7dd56
Sofia Bern <128867240+sofiabern@users.noreply.github.com>
2024-12-06 22:19:38 +0200

    route component approach

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ProductsPage from "./pages/ProductsPage";
-const router = createBrowserRouter([{ path: "/", element: <HomePage /> }, {path: '/products', element: <ProductsPage/>}]);
+
+const routeDefinitions = createRoutesFromElements(
+  <>
+    <Route path="/" element={<HomePage />} />
+    <Route path="/products" element={<ProductsPage />} />
+  </>
+);
+
+const router = createBrowserRouter(routeDefinitions);
 
 function App() {
   return <RouterProvider router={router} />;
