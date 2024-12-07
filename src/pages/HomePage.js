58d3ba04ef9144067fc58c640e56e9382be036e1
Sofia Bern <128867240+sofiabern@users.noreply.github.com>
2024-12-07 17:30:36 +0200

    relative routing

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,13 +4,13 @@ const HomePage = () => {
   const navigate = useNavigate();
 
   function navigateHandler() {
-    navigate("/products");
+    navigate("products");
   }
   return (
     <>
       <h1>My home page</h1>
       <p>
-        Go to <Link to="/products">the list of products</Link>
+        Go to <Link to="products">the list of products</Link>
       </p>
       <p>
         <button onClick={navigateHandler}>Navigate</button>
